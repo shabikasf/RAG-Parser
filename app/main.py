f710d1ec4da6286353bd4b4d13951624b56a1228
Shabika Fathima <85575033+shabikasf@users.noreply.github.com>
2026-06-20 18:35:37 +0530

    Change host to '0.0.0.0' for uvicorn server

--- app/main.py
+++ app/main.py
@@ -24,4 +24,4 @@ app.include_router(query_router, prefix="/api")
 app.include_router(ingest_router, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
